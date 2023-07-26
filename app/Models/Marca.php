8448ef2eec6dfbc5787b3791693926a2adcabc8c
Guilherme Soares <guilherme.sousa009@hotmail.com>
2023-07-26 18:02:39 -0300

    envio de arquivos e salvar em disco + salvando arquivos no local

--- app/Models/Marca.php
+++ app/Models/Marca.php
@@ -13,13 +13,15 @@ class Marca extends Model
     public function rules() {
         return [
             'nome' => 'required|unique:marcas,nome,'.$this->id.'|min:3',
-            'imagem' => 'required'
+            'imagem' => 'required|file|mimes:png'
         ];
     }
     public function feedback() {
         return [
             'required' => 'O campo :attribute é obrigatório',
-            'nome.unique' => 'O nome da marca já existe'
+            'nome.min' => 'O nome deve ter no mínimo 3 caracteres',
+            'nome.unique' => 'O nome da marca já existe',
+            'imagem.mimes' => 'A imagem deve ser do tipo PNG'
         ];
     }
 }
