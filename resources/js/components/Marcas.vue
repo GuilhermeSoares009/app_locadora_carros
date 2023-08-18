06ca628daba952131a946e0f7d72206bdd3d27c3
Guilherme Soares <guilherme.sousa009@hotmail.com>
2023-08-18 16:33:56 -0300

    view home em um componente Vue + Criando o menu de navegação da aplicação +  Criando a rota, a view e o componente marcas +  Criando o card de busca de registros + Criando um componente Vue para encapsular inputs

---
+++ resources/js/components/Marcas.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Marcas</div>
+
+                <div class="card-body">
+                    <div class="form-row">
+                        <div class="col mb-3">
+                            <input-container-component titulo="ID" id="inputId" id-help="inputIdHelp" texto-ajuda="Opcional. Informe o ID do registro">
+                                <input type="number" class="form-control" id="inputId" aria-describedby="inputIdHelp" placeholder="ID">
+                            </input-container-component>
+                        </div>
+
+                        <div class=" col mb-3">
+                            <input-container-component titulo="Nome" id="inputNome" id-help="inputNomeHelp" texto-ajuda="Opcional. Informe o nome da marca">
+                                <input type="text" class="form-control" id="inputNome" aria-describedby="inputNomeHelp" placeholder="Nome">
+                            </input-container-component>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">                    
+                    <button type="submit" class="btn btn-primary btn-sm float-right">Pesquisar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
