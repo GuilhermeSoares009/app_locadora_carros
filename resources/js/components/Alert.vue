0bef326cbd97994ee073a57b1331df78056bf727
Guilherme Soares <guilherme.sousa009@hotmail.com>
2023-08-22 14:25:16 -0300

    Configurando os alertas de sucesso e erro do cadastro de marcas parte 1

---
+++ resources/js/components/Alert.vue
@@ -0,0 +1,17 @@
+<template>
+    <div :class="estilo" role="alert">
+        A simple success alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
+    </div>
+
+</template>
+
+<script>
+    export default {
+        props: ['tipo'],
+        computed: {
+            estilo(){
+                return 'alert alert-'+this.tipo;
+            }
+        }
+    }
+</script>
